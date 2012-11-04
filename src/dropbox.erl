e7c6a25c63459896d13199aea3eb167ffd06c12e
natxo <natxo@keoko.net>
2012-11-04 14:54:14 +0100

    added file_put and file_delete functions.

--- src/dropbox.erl
+++ src/dropbox.erl
@@ -3,6 +3,8 @@
     request_token/2, authorize/5, access_token/4, 
     account_info/4,
     file_get/6,
+    file_put/8,
+    file_delete/6,
     metadata/6
   ]).
 
@@ -30,6 +32,16 @@ account_info(Key, Secret, Token, TokenSecret) ->
 file_get(Key, Secret, Token, TokenSecret, Root, Path) ->
   {ok, {_, _, File}} = oauth:get("https://api-content.dropbox.com/1/files/" ++ Root ++ "/" ++ Path, [], {Key, Secret, hmac_sha1}, Token, TokenSecret),
   File.
+
+file_put(Key, Secret, Token, TokenSecret, Root, Path, ContentType, FileContent) ->
+    {ok, {_, _, Metadata}} = oauth:put("https://api-content.dropbox.com/1/files_put/" ++ Root ++ "/" ++ Path, [], {ContentType, FileContent},  {Key, Secret, hmac_sha1}, Token, TokenSecret),
+    Metadata.
+
+file_delete(Key, Secret, Token, TokenSecret, Root, Path) ->
+    {ok, {_, _, Metadata}} = oauth:post("https://api.dropbox.com/1/fileops/delete", [{"root", Root}, {"path", Path}], {Key, Secret, hmac_sha1}, Token, TokenSecret),
+    Metadata.
+
+
 
 metadata(Key, Secret, Token, TokenSecret, Root, Path) ->
   {ok, {_, _, Metadata}} = oauth:get("https://api.dropbox.com/1/metadata/" ++ Root ++ "/" ++ Path, [], {Key, Secret, hmac_sha1}, Token, TokenSecret),
